e889f777dae086fa91b36daa43aa03e9ab8223c9
RHAEGARII <wakaflockaflaim@gmail.com>
2019-10-19 14:56:40 -0400

    added getColumnsLeft and getColumnsRight

--- great-wall/buildingFunctions.py
+++ great-wall/buildingFunctions.py
@@ -6,3 +6,37 @@ def mirrorCoords(locations):
         #print(output)
     locations+=rightSide
     return locations
+
+
+def getColumnsLeft(startx):
+    x = startx
+    y = startx+14
+    columnIndices = [[x,y]]
+    x+=1
+    columnIndices.append([x,y])
+    y-=1
+    while y>startx:
+        columnIndices.append([x,y])
+        x+=1
+        columnIndices.append([x,y])
+        y-=1
+
+    columnIndices.append([x,y])
+    return columnIndices
+
+
+def getColumnsRight(startx):
+    x = startx
+    y = 27-(x-14)
+    columnIndices = [[x,y]]
+    x-=1
+    columnIndices.append([x,y])
+    y-=1
+    while y>27-startx:
+        columnIndices.append([x,y])
+        x-=1
+        columnIndices.append([x,y])
+        y-=1
+
+    columnIndices.append([x,y])
+    return columnIndices
